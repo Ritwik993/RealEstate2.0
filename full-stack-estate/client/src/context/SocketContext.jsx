7db41f620c39b1243723a5da832e9e4c59f679d7
Ritwik993 <105259259+Ritwik993@users.noreply.github.com>
2024-10-04 12:57:44 +0530

    Update SocketContext.jsx

--- full-stack-estate/client/src/context/SocketContext.jsx
+++ full-stack-estate/client/src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("http://localhost:4000"));
+    setSocket(io("https://realestate2-0-xhgp.onrender.com"));
   }, []);
 
   useEffect(() => {
